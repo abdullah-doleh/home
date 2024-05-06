d745113e6ea3231caa41bc3eaf3119885085b2b8
abdullah doleh <abdallahdoleh98@hotmail.com>
2024-05-06 18:34:41 +0300

    post component

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
     },[])
   return (
     <Provider store={store}>
-    <BrowserRouter basename='home'>
+    <BrowserRouter basename='/'>
     <AlertProvider template={AlertTemplate}  {...options}>
     <Fragment>
       <Alert/>
